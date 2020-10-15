8ff9cac5efd10bb0035f936803e071efdb1e3b37
Nicholas Trujillo <nicholasrtrujillo@gmail.com>
2020-10-15 16:02:33 -0700

    Updates

---
+++ precache-manifest.5e5c03a6976d8583f51114734c1992a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81f9e5199a8a7ace9d3a02e910468615",
+    "url": "/ToDo-app/index.html"
+  },
+  {
+    "revision": "fbe5bc4e178c45024f8c",
+    "url": "/ToDo-app/static/css/main.b6fc3cb2.chunk.css"
+  },
+  {
+    "revision": "2c9a2d069d2d95e573d5",
+    "url": "/ToDo-app/static/js/2.294d8449.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ToDo-app/static/js/2.294d8449.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbe5bc4e178c45024f8c",
+    "url": "/ToDo-app/static/js/main.70a4c120.chunk.js"
+  },
+  {
+    "revision": "b9efc6fc8856e03eb169",
+    "url": "/ToDo-app/static/js/runtime-main.800fd156.js"
+  }
+]);
